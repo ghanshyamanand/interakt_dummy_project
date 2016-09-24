5b50d38dd085973f26b60d59d7425480203c14bf
GHANSHYAM ANAND <ghanshyamanand@fizzysoftware.com>
2016-09-24 23:06:21 +0530

    changes made

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,14 @@
+set :branch, "master"
+set :rails_env, "production"
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+server '139.59.21.145', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
@@ -17,9 +22,9 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+# role :app, %w{deploy@139.59.21.145}, my_property: :my_value
+# role :web, %w{deploy@139.59.21.145}, other_property: :other_value
+# role :db,  %w{deploy@139.59.21.145}
 
 
 
