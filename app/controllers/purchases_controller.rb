8a17096c0c71ce9cf24566066bbbcfe7ffd1718f
GHANSHYAM ANAND <ghanshyamanand@fizzysoftware.com>
2016-10-07 11:37:56 +0530

    changes made

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,7 @@
 class PurchasesController < ApplicationController
+
+  before_action :authenticate_user!
+
   before_action :set_purchase, only: [:show, :edit, :update, :destroy]
 
   # GET /purchases
